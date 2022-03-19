12495e1eba1fd45803be6315a0a407808b81c7bc
Gintarich <71980448+Gintarich@users.noreply.github.com>
2022-03-20 00:13:42 +0200

    Error Handling finished

--- hw3d/Graphics.cpp
+++ hw3d/Graphics.cpp
@@ -5,10 +5,21 @@
 //compiler sets linker settings for us
 #pragma comment(lib,"d3d11.lib")
 
-#define GFX_THROW_FAILED(hrcall) if( FAILED( hr = (hrcall) ) ) throw Graphics::HrException( __LINE__,__FILE__,hr )
-#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException( __LINE__,__FILE__,(hr) )
+// graphics exception checking/throwing macros (some with dxgi infos)
+#define GFX_EXCEPT_NOINFO(hr) Graphics::HrException( __LINE__,__FILE__,(hr) )
+#define GFX_THROW_NOINFO(hrcall) if( FAILED( hr = (hrcall) ) ) throw Graphics::HrException( __LINE__,__FILE__,hr )
 
-Graphics::Graphics( HWND hWnd )
+#ifndef NDEBUG
+#define GFX_EXCEPT(hr) Graphics::HrException( __LINE__,__FILE__,(hr),infoManager.GetMessages() )
+#define GFX_THROW_INFO(hrcall) infoManager.Set(); if( FAILED( hr = (hrcall) ) ) throw GFX_EXCEPT(hr)
+#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException( __LINE__,__FILE__,(hr),infoManager.GetMessages() )
+#else
+#define GFX_EXCEPT(hr) Graphics::HrException( __LINE__,__FILE__,(hr) )
+#define GFX_THROW_INFO(hrcall) GFX_THROW_NOINFO(hrcall)
+#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException( __LINE__,__FILE__,(hr) )
+#endif
+
+Graphics::Graphics(HWND hWnd)
 {
 	//Description structure
 	DXGI_SWAP_CHAIN_DESC sd = { };
@@ -16,27 +27,32 @@ Graphics::Graphics( HWND hWnd )
 	sd.BufferDesc.Height = 0;
 	sd.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
 	sd.BufferDesc.RefreshRate.Numerator = 0;
-	sd.BufferDesc.RefreshRate.Denominator= 0;
-	sd.BufferDesc.Scaling= DXGI_MODE_SCALING_UNSPECIFIED;
+	sd.BufferDesc.RefreshRate.Denominator = 0;
+	sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
 	sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
-	sd.SampleDesc.Count= 1;
+	sd.SampleDesc.Count = 1;
 	sd.SampleDesc.Quality = 0;
 	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	sd.BufferCount = 1;
-	sd.OutputWindow = (HWND)669696;//hWnd;
+	sd.OutputWindow = hWnd;
 	sd.Windowed = TRUE;
 	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
-	sd.Flags =0;
+	sd.Flags = 0;
+
+	UINT swapCreateFlags = 0u;
+#ifndef NDEBUG
+	swapCreateFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
 
 	// for checking results of d3d functions
 	HRESULT hr;
 
 	// create device and front/back buffers, and swap chain and rendering context
-	GFX_THROW_FAILED( D3D11CreateDeviceAndSwapChain(
+	GFX_THROW_INFO(D3D11CreateDeviceAndSwapChain(
 		nullptr,
 		D3D_DRIVER_TYPE_HARDWARE,
 		nullptr,
-		0,
+		swapCreateFlags,
 		nullptr,
 		0,
 		D3D11_SDK_VERSION,
@@ -45,11 +61,11 @@ Graphics::Graphics( HWND hWnd )
 		&m_pDevice,
 		nullptr,
 		&m_pContext
-	) );
+	));
 	// gain access to texture subresource in swap chain (back buffer)
 	ID3D11Resource* pBackBuffer = nullptr;
-	GFX_THROW_FAILED( m_pSwap->GetBuffer( 0,__uuidof(ID3D11Texture2D),reinterpret_cast<void**>(&pBackBuffer) ) );
-	GFX_THROW_FAILED( m_pDevice->CreateRenderTargetView( pBackBuffer,nullptr,&m_pTarget ) );
+	GFX_THROW_INFO(m_pSwap->GetBuffer(0, __uuidof( ID3D11Resource ), reinterpret_cast< void** >( &pBackBuffer )));
+	GFX_THROW_INFO(m_pDevice->CreateRenderTargetView(pBackBuffer, nullptr, &m_pTarget));
 	pBackBuffer->Release();
 }
 
@@ -75,43 +91,63 @@ Graphics::~Graphics()
 
 void Graphics::EndFrame()
 {
+#ifndef NDEBUG
+	infoManager.Set();
+#endif
+
 	HRESULT hr;
-	if( FAILED( hr = m_pSwap->Present( 1u,0u ) ) )
+	if( FAILED(hr = m_pSwap->Present(1u, 0u)) )
 	{
 		if( hr == DXGI_ERROR_DEVICE_REMOVED )
 		{
-			throw GFX_DEVICE_REMOVED_EXCEPT( m_pDevice->GetDeviceRemovedReason() );
+			throw GFX_DEVICE_REMOVED_EXCEPT(m_pDevice->GetDeviceRemovedReason());
 		}
 		else
 		{
-			GFX_THROW_FAILED( hr );
+			throw GFX_EXCEPT(hr);
 		}
 	}
 }
 
-void Graphics::ClearBuffer( float red,float green,float blue ) noexcept
+void Graphics::ClearBuffer(float red, float green, float blue) noexcept
 {
 	const float color[] = { red,green,blue,1.0f };
-	m_pContext->ClearRenderTargetView( m_pTarget,color );
+	m_pContext->ClearRenderTargetView(m_pTarget, color);
 }
 
 
 // Graphics exception stuff
-Graphics::HrException::HrException( int line,const char * file,HRESULT hr ) noexcept
+Graphics::HrException::HrException(int line, const char* file, HRESULT hr, std::vector<std::string> infoMsgs) noexcept
 	:
-	Exception( line,file ),
-	hr( hr )
-{}
+	Exception(line, file),
+	hr(hr)
+{
+	// join all info messages with newlines into single string
+	for( const auto& m : infoMsgs )
+	{
+		info += m;
+		info.push_back('\n');
+	}
+	// remove final newline if exists
+	if( !info.empty() )
+	{
+		info.pop_back();
+	}
+}
 
 const char* Graphics::HrException::what() const noexcept
 {
 	std::ostringstream oss;
 	oss << GetType() << std::endl
 		<< "[Error Code] 0x" << std::hex << std::uppercase << GetErrorCode()
-		<< std::dec << " (" << (unsigned long)GetErrorCode() << ")" << std::endl
+		<< std::dec << " (" << ( unsigned long )GetErrorCode() << ")" << std::endl
 		<< "[Error String] " << GetErrorString() << std::endl
-		<< "[Description] " << GetErrorDescription() << std::endl
-		<< GetOriginString();
+		<< "[Description] " << GetErrorDescription() << std::endl;
+	if( !info.empty() )
+	{
+		oss << "\n[Error Info]\n" << GetErrorInfo() << std::endl << std::endl;
+	}
+	oss << GetOriginString();
 	whatBuffer = oss.str();
 	return whatBuffer.c_str();
 }
@@ -128,16 +164,18 @@ HRESULT Graphics::HrException::GetErrorCode() const noexcept
 
 std::string Graphics::HrException::GetErrorString() const noexcept
 {
-	return DXGetErrorString( hr );
+	return DXGetErrorString(hr);
 }
-
 std::string Graphics::HrException::GetErrorDescription() const noexcept
 {
-	char buf[512];
-	DXGetErrorDescription( hr,buf,sizeof( buf ) );
+	char buf[ 512 ];
+	DXGetErrorDescription(hr, buf, sizeof(buf));
 	return buf;
 }
-
+std::string Graphics::HrException::GetErrorInfo() const noexcept
+{
+	return info;
+}
 
 const char* Graphics::DeviceRemovedException::GetType() const noexcept
 {
