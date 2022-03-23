498b188b0e7c5c0092918128fdbac08dfa8311f3
Gintarich <71980448+Gintarich@users.noreply.github.com>
2022-03-23 23:56:01 +0200

    Drawable/Bindable System END

--- hw3d/Box.h
+++ hw3d/Box.h
@@ -8,11 +8,9 @@ public:
 		std::uniform_real_distribution<float>& adist,
 		std::uniform_real_distribution<float>& ddist,
 		std::uniform_real_distribution<float>& odist,
-		std::uniform_real_distribution<float>& rdist);
-	void Update(float dt) noexcept override;
 		std::uniform_real_distribution<float>& rdist,
-		std::uniform_real_distribution<float>& bdist );
-	void Update( float dt ) noexcept override;
+		std::uniform_real_distribution<float>& bdist);
+	void Update(float dt) noexcept override;
 	DirectX::XMMATRIX GetTransformXM() const noexcept override;
 private:
 	// positional
